81d354140319f2488c9b934ed6bd2ebbb4a7353b
twkang43 <twkang43@gmail.com>
2023-12-20 14:19:47 +0900

    update model path

--- py_src/franka_valve.py
+++ py_src/franka_valve.py
@@ -1,4 +1,5 @@
 import os
+import json
 import argparse
 
 from models.tqc.tqc import TQC
@@ -12,6 +13,32 @@ def check_valid_object(object):
     if object not in valid_values:
         raise ValueError(f"Invalid object. Valid values are {valid_values}")
 
+# Train mode
+def get_train_model_path():
+    base_path = os.path.join(HOME, "models", "tqc", "model")
+    models = os.listdir(base_path)
+    models.sort()
+
+    try:
+        model_name = f"model_{int(models[-1][-1])+1}"
+        os.makedirs(os.path.join(base_path, model_name), exist_ok=True)
+        return model_name
+    except ValueError:
+        model_name = "model_1"
+        os.makedirs(os.path.join(base_path, model_name), exist_ok=True)
+        return model_name
+
+# Evaluation mode
+def get_eval_model_path(model_index):
+    if model_index == "default_model":
+        return "default_model"
+    
+    try:
+        int_index = int(model_index)
+        return f"model_{int_index}"
+    except ValueError:
+        raise ValueError("Cannot change an input index to integer.")
+    
 # Train mode
 def train_franka_valve(
         rw_acc        = 3,
@@ -47,9 +74,40 @@ def train_franka_valve(
     :param n_quantiles: The number of quantiles
     '''
     check_valid_object(object)
+    model_path = get_train_model_path()
+
+    # Save hyperparameters
+    env_hyperparameters = {
+                            "rw_acc" : rw_acc,
+                            "rw_c" : rw_c,
+                            "rw_b" : rw_b,
+                            "rw_gr" : rw_gr,
+                            "history" : history,
+                            "object" : object
+                          }
+    model_hyperparameters = {
+                                "max_timesteps" : max_timesteps,
+                                "batch_size" : batch_size,
+                                "save_freq" : save_freq,
+                                "n_critics" : n_critics,
+                                "n_quantiles" : n_quantiles
+                            }
+    
+    hyperparameters = {
+                        "environments" : env_hyperparameters,
+                        "models" : model_hyperparameters
+                      }
+    
+    os.chdir(os.path.join(HOME, "models", "tqc", "model", model_path))
+    with open("hyperparameters.json", 'w') as f:
+        json.dump(hyperparameters, f, ensure_ascii=False, indent=4)
+    os.chdir(HOME)
+
+    # Load env & model
     env = Fr3_tqc(rw_acc, rw_c, rw_b, rw_gr, history, object)
     policy_kwargs = dict(n_critics=n_critics, n_quantiles=n_quantiles)
-    model = TQC(env, policy_kwargs, max_timesteps, batch_size, save_freq)
+    model = TQC(env, policy_kwargs, max_timesteps, batch_size, save_freq, model_path, epochs_index="-1.0")
+
     model.train()
 
 # Evaluation mode
@@ -64,7 +122,9 @@ def eval_franka_valve(
         batch_size    = 256, 
         save_freq     = 1e5,
         n_critics     = 5,
-        n_quantiles   = 25
+        n_quantiles   = 25,
+        model_index   = "default_model",
+        epochs_index  = "0.0"
     ):
     '''
     Evaluate franka_valve
@@ -87,9 +147,12 @@ def eval_franka_valve(
     :param n_quantiles: The number of quantiles
     '''
     check_valid_object(object)
+    model_path = get_eval_model_path(model_index)
+
     env = Fr3_tqc(rw_acc, rw_c, rw_b, rw_gr, history, object)
     policy_kwargs = dict(n_critics=n_critics, n_quantiles=n_quantiles)
-    model = TQC(env, policy_kwargs, max_timesteps, batch_size, save_freq)
+    model = TQC(env, policy_kwargs, max_timesteps, batch_size, save_freq, model_path, epochs_index)
+
     model.eval()
 
 if __name__ == "__main__":
